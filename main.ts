7004cc49a74155ceaf38ef3d5e2fca7766792d23
Lars Christian Raae <36758964+Ctrl-Alt-Lars@users.noreply.github.com>
2026-08-03 09:55:41 +0200

    Dino game with fly and cactus complete

--- main.ts
+++ main.ts
@@ -9,6 +9,8 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
 sprites.onDestroyed(SpriteKind.Projectile, function (sprite) {
     info.changeScoreBy(1)
 })
+let exit2 = 0
+let exit = 0
 let projectile: Sprite = null
 let TRex: Sprite = null
 scene.setBackgroundColor(13)
@@ -20,4 +22,13 @@ game.onUpdateInterval(1500, function () {
     projectile = sprites.createProjectileFromSide(assets.image`cactus`, randint(-200, -120), 0)
     tiles.placeOnTile(projectile, tiles.getTileLocation(9, 5))
     projectile.setFlag(SpriteFlag.AutoDestroy, true)
+    exit += 1
+    if (exit > 10) {
+        exit2 = randint(1, 2)
+        if (exit2 == 2) {
+            pause(100)
+            projectile = sprites.createProjectileFromSide(assets.image`fly`, randint(-120, -100), 0)
+            tiles.placeOnTile(projectile, tiles.getTileLocation(9, 3))
+        }
+    }
 })
